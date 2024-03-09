4dffae49fc0f0c43aca279b45d9b1dcb6d93f5f2
Chrislopz <clopezl45@miumg.edu.gt>
2024-03-09 12:53:26 -0600

    Primer commit

---
+++ productos/productos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace productos
+{
+    class Promedio
+    {
+        public static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.Write("Bienvenido ");
+
+            Console.WriteLine("Por favor ingresa 3 productos");
+
+            Console.Write("Ingresa el precio del primer producto: ");
+            string dato = Console.ReadLine();
+            int n1 = int.Parse(dato);
+
+            Console.Write("Ingresa el precio del primer producto: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+
+            Console.Write("Ingresa el precio del primer producto: ");
+            dato = Console.ReadLine();
+            int n3 = int.Parse(dato);
+
+            int suma = n1 + n2 + n3;
+
+            if (suma > 100)
+            {
+                double v = suma * 0.15;
+                Console.WriteLine("Su descuento es: " +  v);
+            }
+            
+
+            Console.WriteLine("El total de su compra es: {0}", suma);
+            
+
+            Console.Read();
+        }
+    }
+}
---
+++ serie3 programa1/serie3 programa1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace programa1
+{
+    class correo
+    {
+
+        public static void Main(string[] args)
+        {
+
+            string gmail;
+            Console.WriteLine("Hola, Bienvenido");
+            Console.Write("Por favor, ingresa un correo electrónico(gmail):");
+            gmail = Console.ReadLine();
+            Console.WriteLine("Bienvenido " + gmail);
+
+            if (gmail == @gmail)
+            {
+                Console.WriteLine(gmail + "@miumg.edu.gt");
+            }
+            
+
+
+        }
+    }
+}
